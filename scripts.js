cdb92806ea24bc1fc325710bbf160e434e66a4a8
Russell Bentulan <russellbentulan@gmail.com>
2020-02-22 23:49:46 -0500

    Getting the turtle elements from the dom and adding properties

--- scripts.js
+++ scripts.js
@@ -1,20 +1,73 @@
-const getRandomWords = () => {
-  return fetch(RANDOM_WORDS_API_URL)
-    .then(res => res.json())
-    .then(data => data.reduce((words,item) => { 
-      // Clean up word list to only get acceptable words
-      if (/^[a-z]+$/.test(item.word)) {
-        words.push(item.word)
-      }
-      return words
-    }, []))
+class Turtle {
+  constructor(element, chickenDinner = false) {
+    this.currentPosition = "middle"
+    this.winner = chickenDinner
+    this.domElement = element
+  }
+
+  setPosition = newPosition => {
+    if (this.currentPosition != newPosition) {
+      this.domElement.classList.remove(this.currentPosition);
+      this.domElement.classList.add(newPosition);
+      this.currentPosition = newPosition;
+    }
+  }
+
+  clickHandler = () => {
+    if (this.winner) {
+      console.log("Winner winner!")
+    } else {
+      console.log("Loser loser!!")
+    }
+  }
 }
 
-const fillWordList = async () => {
-  const words = await getRandomWords();
-  console.log(words);
-  // const filteredWords = words.map(word => {
-  //   if (/^[a-z]+$/.test(item.word))
-  // })
+// Globals
+const headingElement = document.getElementById('heading')
+
+const turtleOne = new Turtle(document.getElementById("turtle1"))
+const turtleTwo = new Turtle(document.getElementById("turtle2"), true)
+const turtleThree = new Turtle(document.getElementById("turtle3"))
+
+turtles = [turtleOne, turtleTwo, turtleThree]
+
+const initialTurtlePositions = () => {
+  turtles.forEach((turtle, i) => {
+    const positions = {
+      0: "left",
+      1: "middle",
+      2: "right"
+    }
+
+    turtle.setPosition(positions[i])
+  })
 }
-fillWordList();
+
+// Initializing game
+const init = () => {
+  initialTurtlePositions()
+}
+init();
+
+// Getting words
+// const getRandomWords = () => {
+//   return fetch(RANDOM_WORDS_API_URL)
+//     .then(res => res.json())
+//     .then(data => data.reduce((words,item) => { 
+//       // Clean up word list to only get acceptable characters
+//       // Also gets rid of proper nouns
+//       if (/^[a-z]+$/.test(item.word)) {
+//         words.push(item.word)
+//       }
+//       return words
+//     }, []))
+// }
+
+// const wordList = [];
+
+// // Adds new words to the wordList if it's below 10 words
+// const fillWordList = async () => {
+//   if (wordList.length < 10) {
+//     wordList.push(...await getRandomWords())
+//   }
+// }
